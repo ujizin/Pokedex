d150b4c6933a027067e13f60fb3083d46dafc21d
DevLucasYuji <lucasyujideveloper@gmail.com>
2020-03-20 01:16:23 -0300

    resolving conflicts (bug)

--- test/app/module/home/home_page_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-main() {
-  testWidgets('HomePage has title', (WidgetTester tester) async {
-    //  await tester.pumpWidget(buildTestableWidget(HomePage(title: 'Home')));
-    //  final titleFinder = find.text('Home');
-    //  expect(titleFinder, findsOneWidget);
-  });
-}
